38f9d8f2adec097bb019c109f8f5ee75d0bec4ad
Nirav Dadhaniya <nvd8866@gmail.com>
2023-04-15 06:00:58 +0530

    Minor changes

--- src/MyIntroduction/components/IntroductionHeader/IntroductionHeader.js
+++ src/MyIntroduction/components/IntroductionHeader/IntroductionHeader.js
@@ -7,7 +7,7 @@ function IntroductionHeader () {
         <div className="header_container">
             <div className="header_name">
                 <div className="name_logo">
-                    <img src={Logo} width={40} height={40}/>
+                    <img src={Logo} width={40} height={40} alt="Logo"/>
                 </div>
                 <div className="name_text">
                     Nirav Dadhaniya
