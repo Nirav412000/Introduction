648bd67bce617c2debba394dc83d7cc28c660ddf
Nirav Dadhaniya <nvd8866@gmail.com>
2023-04-18 17:30:11 +0530

    refactor sectionheader component

--- src/MyIntroduction/CommonComponents/SectionHeader/SectionHeader.js
+++ src/MyIntroduction/CommonComponents/SectionHeader/SectionHeader.js
@@ -5,8 +5,10 @@ function SectionHeader(props) {
     const { sectionTitle, subTitle } = props;
     return(
         <div className="titleContainer">
-            <div className="title">{sectionTitle}
-                <span className="nameTitle">{subTitle}</span>
+            <div className="title">{sectionTitle}</div>
+            <div className="nameTitle">
+            <div className="title" style={{opacity: 0, height: 0}}>{sectionTitle}</div>
+                {subTitle}
             </div>
         </div>
     )
